50802bf6f24c43d28eba6158dbcc6c207179ac80
n8feldman <nfeldman1995@gmail.com>
2026-03-21 21:54:56 -0400

    teachers

--- server/models/band.js
+++ server/models/band.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose'
 
 const bandSchema = new mongoose.Schema({
-	name: { type: String, required: true },
+	name: {
+		type: String,
+		required: true,
+	},
+	teacherId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Teacher',
+		required: true,
+	},
 })
 
 const Band = mongoose.model('Band', bandSchema)
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 
 import students from './routes/student.js'
 import bands from './routes/band.js'
+import teachers from './routes/teacher.js'
 
 const app = express()
 const PORT = process.env.PORT || 5050
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/students', students);
 app.use('/bands', bands)
+app.use('/teachers', teachers)
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
